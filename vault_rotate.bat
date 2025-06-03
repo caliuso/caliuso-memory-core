804b39852466907f66cb8cf742f24324d8111364
Caliuso <marksmailbox714@gmail.com>
2025-06-03 01:53:16 -0700

    🔁 Auto-sync update 2025-06-03T08:53:15.915265

--- vault_rotate.bat
+++ vault_rotate.bat
@@ -1,2 +1,9 @@
-cd C:\Users\Administrator\Desktop\CALIUSODEBUG\caliuso-memory-core
-python rotate_backup.py
+@echo off
+cd /d "C:\Users\Administrator\Desktop\CALIUSODEBUG\caliuso-memory-core"
+
+nssm start caliuso-memory-api
+nssm start caliuso-autosync
+nssm start caliuso-immortal-watchdog
+
+echo IMMORTAL BOOTUP COMPLETE
+pause
